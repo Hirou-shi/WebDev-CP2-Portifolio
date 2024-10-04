6bced290952cd204a213137b3317dfab7b23b887
Rodrigo Hiroshi <rodrigohiroshiwatanabe@gmail.com>
2024-10-04 12:46:21 -0300

    ok

--- hiro-portfolio/src/pages/Home.jsx
+++ hiro-portfolio/src/pages/Home.jsx
@@ -2,10 +2,13 @@
 export default function Home(){
     return(
         <body className="">
-            <section className="text-white">
-                <h1>Hi! I'm</h1>
-                <h1>Hiro</h1>
-                <h3><>Full-stack Developer</></h3>
+            <section className="text-white grid place-items-center p-14">
+                <h1 className="font-bold text-[96px]">Hi! I'm</h1>
+                <h1 className="text-[#3523FF] font-bold text-[120px]">Hiroshi</h1>
+                <h3 className="font-semibold text-[28px]">Full-stack Developer</h3>
+            </section>
+            <section>
+                <a href="https://github.com/Hirou-shi"><img src="./assets/Github.png" alt="" /></a>
             </section>
         </body>
     );
