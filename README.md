bdec0269df8b061f58d902c5123261628d3b47c2
Rodrigo Hiroshi <rodrigohiroshiwatanabe@gmail.com>
2024-10-06 21:58:45 -0300

    Ajustes finais

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 Istalei Tailwind
 Instalei a biblioteca de icones da Lucide-React
 Instalei a biblioteca de icones da React icons
-https://eufujita.vercel.app/#sobremim
-https://github.com/fujita022/EcoMarina.git
+
--- hiro-portfolio/src/pages/Projects.jsx
+++ hiro-portfolio/src/pages/Projects.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Cards from '../components/Card';
 
 export default function Project() {
     return(
@@ -9,9 +8,37 @@ export default function Project() {
                     <h1 className='text-white'>My recents</h1>
                     <h1 className='text-[#3523FF]'>Projects</h1>
                 </div>
-                <div className='pb-[25%]'>
-                <   Cards />
-                </div>
+                <div className='flex flex-wrap justify-center gap-6 p-4'>
+                    <div className=" bg-black/50 shadow-lg rounded-lg overflow-hidden max-w-xs">
+                            <img
+                                src='/Global.png'
+                                alt='Imagem do site global'
+                                className="w-full h-48 object-cover"
+                                />
+                        <div className="p-4">
+                            <h2 className="text-white text-xl font-bold mb-2">Global</h2>
+                            <p className="text-gray-700">This have CSS, Javascripts and HTML basics functions.</p>
+                            <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors">
+                            Learn More
+                            </button>
+                        </div>
+                    </div>
+                    <div className="bg-black/50 shadow-lg rounded-lg overflow-hidden max-w-xs">
+                            <img
+                                src='/Airbnb.png'
+                                alt='Imagem do site global'
+                                className="w-full h-48 object-cover"
+                                />
+
+                        <div className="p-4">
+                            <h2 className="text-white text-xl font-bold mb-2">Airbnb Clone</h2>
+                            <p className="text-gray-700">This have CSS, Javascripts and HTML basics functions.</p>
+                            <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors">
+                            Learn More
+                            </button>
+                        </div>
+                    </div>
+                    </div>
                 
             </body>
         </>
