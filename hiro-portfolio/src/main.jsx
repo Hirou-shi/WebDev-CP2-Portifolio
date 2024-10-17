05a5e69c0d96d14df4f7bb00931c107e2d8fa553
Rodrigo Hiroshi <161664438+Hirou-shi@users.noreply.github.com>
2024-10-16 22:41:23 -0300

    up

--- hiro-portfolio/src/main.jsx
+++ hiro-portfolio/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+  import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
 import Home from './pages/Home.jsx';
 import Contact from './pages/Contact.jsx';
 import Projects from './pages/Projects.jsx';
